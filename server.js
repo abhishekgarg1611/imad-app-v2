64e3672c72052d03ced90a9a91e589411109981e
abhishekgarg1611 <gargabhishek1one@gmail.com>
2017-02-02 19:56:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,14 +7,21 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});(
-app.get('/ui/style.css', function (req, res) {
+});
+
 app.get('/article-one', function (req, res) {
-    res.send('article one requested and will be served here');
+    res.send('article-one requested and will be served here');
 });
-    
+
+app.get('/article-one', function (req, res) {
+    res.send('article-two requested and will be served here');
+});
+app.get('/article-one', function (req, res) {
+     res.send('article-two requested and will be served here');
+});
+
 res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
