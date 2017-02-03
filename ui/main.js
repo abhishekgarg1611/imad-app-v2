42e1465ab154c60eae837cfbed82ae1202e78710
abhishekgarg1611 <gargabhishek1one@gmail.com>
2017-02-03 17:05:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,8 @@
-console.log('Loaded!');
+
+})
+console.log('Loaded
+});
  alert('Hi,I\'m Javascript');
+ });
+ app.get('ui/main.js',function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'main.js'));
