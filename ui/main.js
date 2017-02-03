4b056716a6473ca860bceab9d50dfd700b908730
abhishekgarg1611 <gargabhishek1one@gmail.com>
2017-02-03 17:33:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,37 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
-})
-console.log('Loaded
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
- alert('Hi,I\'m Javascript');
- });
- app.get('ui/main.js',function (req, res) {
+
+app.get('/article-one', function (req, res) {
+     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/article-3', function (req, res) {
+     res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('ui/main.js',function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
