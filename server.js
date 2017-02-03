73d925f8b994cbc4ee26404f47568abbdaa72c7e
abhishekgarg1611 <gargabhishek1one@gmail.com>
2017-02-03 20:06:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/article-3', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));
 });
+
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
