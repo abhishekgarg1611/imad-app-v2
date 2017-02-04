1d56ad8b3082690ae7b84c32ab0f9be6f6f0c37f
abhishekgarg1611 <gargabhishek1one@gmail.com>
2017-02-04 23:09:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ app.get('ui/style.css',function (req,rec){
     res.sendfile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-ccapp.get('ui/main.js',function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
